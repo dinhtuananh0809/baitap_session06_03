96c1dbe2bd306ca2b34d75c683602805c77ceff3
DINH TUAN ANH <dinhtuananh0809@gmail.com>
2023-05-03 12:55:18 +0900

    add new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import Index from './Components/Index';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Services from './Components/Services';
+import Elements from './Components/Elements';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Link to='/Index'></Link>
+      <Link to='/About'></Link>
+      <Link to='/Contact'></Link>
+      <Link to='/Elements'></Link>
+      <Link to='/Service'></Link>
+      <Routes>
+
+        <Route path='/Index' element={<Index/>}></Route>
+        <Route path='/About' element={<About/>}></Route>
+        <Route path='/Contact' element={<Contact/>}></Route>
+        <Route path='/Element' element={<Elements/>}></Route>
+        <Route path='/Service' element={<Services/>}></Route>
+      </Routes>
     </div>
   );
 }
